fc84d011679ad707a6156ca5bafef0054863ffe1
Luis Henrique <luis@prover.net.br>
2024-08-25 16:36:39 -0300

    feat: dashboard header and new table

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,9 @@
 import { Container } from "@/components/container"
+import { TicketItem } from "@/app/dashboard/components/ticket"
 import { getServerSession } from "next-auth"
 import { authOptions } from "@/lib/auth"
 import { redirect } from "next/navigation"
+import Link from "next/link"
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions)
@@ -12,7 +14,28 @@ export default async function Dashboard() {
 
   return (
     <Container>
-      <h1>Dashboard</h1>
+      <main className="mt-9 mb-2">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold">Chamados</h1>
+          <Link href="/dashboard/new" className="bg-blue-500 px-4 py-1 rounded text-white">
+            Abrir chamado
+          </Link>
+        </div>
+
+        <table className="min-w-full my-2">
+          <thead>
+            <tr>
+              <th className="font-medium text-left pl-1">CLIENTE</th>
+              <th className="font-medium text-left hidden sm:block">DATA CADASTRO</th>
+              <th className="font-medium text-left">STATUS</th>
+              <th className="font-medium text-left">#</th>
+            </tr>
+          </thead>
+          <tbody>
+            <TicketItem />
+          </tbody>
+        </table>
+      </main>
     </Container>
   )
 }
