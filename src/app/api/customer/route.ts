b05cd12c633a2214ff8dccbe7ff51cddb2aa9e89
Luis Henrique <luis@prover.net.br>
2024-08-29 14:19:06 -0300

    delete customer route

--- src/app/api/customer/route.ts
+++ src/app/api/customer/route.ts
@@ -4,6 +4,34 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import prismaClient from "@/lib/prisma";
 
+export async function DELETE (request: Request) {
+
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const { searchParams } = new URL(request.url);
+  const userId = searchParams.get("id");
+
+  if (!userId) {
+    return NextResponse.json({ error: "User id is required" }, { status: 400 });
+  }
+
+  try {
+    await prismaClient.customer.delete({
+      where: {
+        id: userId as string
+      }
+    })
+    return NextResponse.json({ message: "Customer deleted successfully" });
+  } catch (error) {
+    return NextResponse.json({ error: "Failed delete customer" }, { status: 400 });
+  }
+
+}
+
 export async function POST (request: Request) {
   const session = await getServerSession(authOptions);
 
