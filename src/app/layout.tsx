c2116d43c22b3b7b47915f35e7408f24f02b630c
Luis Henrique <luis@prover.net.br>
2024-08-30 19:53:31 -0300

    new modal ticket

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,32 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { Header } from "@/components/header";
-import { AuthProvider } from "@/providers/auth";
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import { Header } from "@/components/header"
+import { AuthProvider } from "@/providers/auth"
+import { ModalProvider } from "@/providers/modal"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "ServiceFlow - Seu sistema de gerenciamento.",
   description: "Gerencie seus clientes e atendimentos de forma fácil!",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <AuthProvider>
-          <Header />
-          {children}
+          <ModalProvider>
+            <Header />
+            {children}
+          </ModalProvider>
         </AuthProvider>
       </body>
     </html>
-  );
+  )
 }
