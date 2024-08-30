693eee81003f8906d62996641cd4ba85fb1ae06c
Luis Henrique <luis@prover.net.br>
2024-08-30 16:33:43 -0300

    create ticket page

--- src/app/dashboard/new/page.tsx
+++ src/app/dashboard/new/page.tsx
@@ -19,6 +19,29 @@ export default async function NewTicket() {
     },
   })
 
+  async function handleRegisterTicket(formData: FormData) {
+    "use server"
+
+    const name = formData.get("name")
+    const description = formData.get("description")
+    const customerId = formData.get("customer")
+
+    if (!name || !description || !customerId) {
+      return
+    }
+
+    await prismaClient.ticket.create({
+      data: {
+        name: name as string,
+        description: description as string,
+        customerId: customerId as string,
+        status: "ABERTO",
+        userId: session?.user.id,
+      },
+    })
+    redirect("/dashboard")
+  }
+
   return (
     <Container>
       <main className="mt-9 mb-2">
@@ -32,11 +55,12 @@ export default async function NewTicket() {
           <h1 className="text-3xl font-bold">Novo chamado</h1>
         </div>
 
-        <form className="flex flex-col mt-6">
+        <form className="flex flex-col mt-6" action={handleRegisterTicket}>
           <label className="mb-1 font-medium text-lg">Nome do chamado</label>
           <input
             type="text"
             placeholder="Digite o nome do chamado"
+            name="name"
             required
             className="w-full border-2 rounded-md px-2 mb-2 h-11"
           />
@@ -44,6 +68,7 @@ export default async function NewTicket() {
             Descreva o problema
           </label>
           <textarea
+            name="description"
             placeholder="Descreva o problema..."
             required
             className="w-full border-2 rounded-md px-2 mb-2 h-24 resize-none"
@@ -53,7 +78,10 @@ export default async function NewTicket() {
               <label className="mb-1 font-medium text-lg">
                 Selecione o cliente
               </label>
-              <select className="w-full border-2 rounded-md px-2 mb-2 h-11 resize-none bg-white">
+              <select
+                className="w-full border-2 rounded-md px-2 mb-2 h-11 resize-none bg-white"
+                name="customer"
+              >
                 {customers.map((customer) => (
                   <option key={customer.id} value={customer.id}>
                     {customer.name}
